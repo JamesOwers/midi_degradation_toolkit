12077d60bfb2f6743b6851af3c5c0253d68d4e0a
JamesOwers <james.f.owers+github@gmail.com>
2019-06-28 16:03:09 +0100

    Update docstring

--- mdtk/__init__.py
+++ mdtk/__init__.py
@@ -1,4 +1,7 @@
-"""init for top level package"""
+"""
+Tools to generate datasets of Altered and Corrupted MIDI Excerpts -`ACME`
+datasets.
+"""
 __all__ = ["data_structures", "degradations", "download", "pytorch_datasets"]
 __author__ = "James Owers"
 __credits__ = ["James Owers", "Andrew McLeod"]
