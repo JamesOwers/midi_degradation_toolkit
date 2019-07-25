4b9b0e7bde3a1d04c8165e53a941408e3e97d21f
JamesOwers <james.f.owers+github@gmail.com>
2019-07-25 15:30:36 +0100

    Finished midi download method
    This nearly finishes #18d84k. Still need to write some tests, and
    put a bit of the script together.

--- mdtk/downloaders.py
+++ mdtk/downloaders.py
@@ -5,79 +5,166 @@ new class which extends DataDownloader, and write an accompanying test in
 ./tests/test_downloads.py
 """
 import os
+import sys
 import shutil
 import urllib
+import warnings
+import zipfile
+import glob
+from tqdm import tqdm
 
 
 
 USER_HOME = os.path.expanduser('~')
-DEFAULT_CACHE_LOC = os.path.join(USER_HOME, '.mdtk_cache')
+DEFAULT_CACHE_PATH = os.path.join(USER_HOME, '.mdtk_cache')
 
 
 
-class DataDownloader:
-    """Base class for data downloaders"""
-    def __init__(self, cache_loc=DEFAULT_CACHE_LOC, 
-                 midi_output_loc=None, csv_output_loc=None):
-        self.dataset_name = self.__class__.__name__
-        self.download_urls = []
-        self.midi_locations = []
-        self.csv_locations = []
-        self.cache_loc = cache_loc
-        self.output_loc = {'midi': midi_output_loc, 'csv': csv_output_loc}
-        
-    
-    def get_output_and_cache_loc(self, data_type, output_loc=None,
-                                 cache_loc=None):
-        """Convenience method to allow methods to take the class specified
-        output and cache locations, or locations specified in the method
-        call arguments."""
-        if output_loc is None:
-            output_loc = self.output_loc[data_type]
-        if cache_loc is None:
-            cache_loc = self.cache_loc
-        return output_loc, cache_loc
-    
-    
-    @staticmethod
-    def download_file(source, dest, verbose=True, overwrite=None):
+def download_file(source, dest, verbose=True, overwrite=None):
         """Get a file from a url and save it locally"""
         if verbose:
             print(f'Downloading {source} to {dest}')
         if os.path.exists(dest):
             if overwrite is None:
-                print(f'WARNING: {dest} already exists, not downloading')
+                warnings.warn(f'WARNING: {dest} already exists, not '
+                              'downloading', category=UserWarning)
                 return
             if not overwrite:
                 raise OSError(f'{dest} already exists')
         try:
             urllib.request.urlretrieve(source, dest)
         except urllib.error.HTTPError as e:
-            print(f'Url {source} does not exist')
+            print(f'Url {source} does not exist', file=sys.stderr)
             raise e
-            
+
+
+def make_directory(path, overwrite=None, make_parents=True, verbose=True):
+        """Convenience function to create a directory and handle cases where
+        it already exists.
+        
+        Args
+        ----
+        path: str
+            The path of the directory to create
+        overwrite: boolean or None
+            If the path already exists, if overwrite is: True - delete the 
+            existing path; False - return error; None - leave the existing
+            path as it is and throw a warning    
+        make_parents: bool
+            Whether to create the parent folders of a dir if they don't exist
+        verbose: bool
+            Verbosity of printing
+        """
+        if verbose:
+            print(f'Making directory at {path}')
+        
+        if make_parents:
+            mkdir = os.makedirs
+        else:
+            mkdir = os.mkdir
+        
+        try:
+            mkdir(path)
+        except FileExistsError as e:
+            if overwrite is True:
+                print(f'Deleting existing directory: {path}')
+                shutil.rmtree(path)
+                mkdir(path)
+            elif overwrite is None:
+                warnings.warn(f'WARNING: {path} already exists, writing files '
+                      'within here only if they do not already exist.',
+                      category=UserWarning)
+            elif overwrite is False:
+                raise e
+            else:
+                raise ValueError('overwrite should be boolean or None, not '
+                                 f'"{overwrite}"')
+
+
+def extract_zip(zip_path, out_path, overwrite=None, verbose=True):
+    """Convenience function to extract zip file to out_path.
+    TODO: make work for all types of zip files."""
+    if verbose:
+        print(f'Extracting {zip_path} to {out_path}')
+    dirname = os.path.splitext(os.path.basename(zip_path))[0]
+    extracted_path = os.path.join(out_path, dirname)
+    if os.path.exists(extracted_path):     
+        if overwrite is True:
+            warnings.warn(f'Deleting existing directory: {extracted_path}')
+            shutil.rmtree(extracted_path)
+        elif overwrite is None:
+            warnings.warn(f'{extracted_path} already exists. Assuming this '
+                          'zip has already been extracted, not extracting.',
+                  category=UserWarning)
+            return extracted_path
+        elif overwrite is False:
+            raise FileExistsError('{path} already exists')
+    
+    with zipfile.ZipFile(zip_path, 'r') as zz:
+        zz.extractall(path=out_path)
+    return extracted_path
+
+
+
+class DataDownloader:
+    """Base class for data downloaders"""
+    def __init__(self, cache_path=DEFAULT_CACHE_PATH, 
+                 midi_output_path=None, csv_output_path=None):
+        self.dataset_name = self.__class__.__name__
+        self.download_urls = []
+        self.midi_paths = []
+        self.csv_paths = []
+        self.downloaded = False
+        self.extracted = False
+        self.cache_path = cache_path
+        self.output_path = {'midi': midi_output_path, 'csv': csv_output_path}
         
     
-    def download_midi(self, output_loc=None, cache_loc=None):
-        """Downloads the MIDI data to output_loc"""
-        output_loc, cache_loc = self.get_output_and_cache_loc(
+    def get_output_and_cache_paths(self, data_type, output_path=None,
+                                   cache_path=None):
+        """Convenience method to allow methods to take the class specified
+        output and cache pathations, or pathations specified in the method
+        call arguments."""
+        if output_path is None:
+            output_path = self.output_path[data_type]
+            if output_path is None:
+                raise ValueError('You need to state the output_path on either '
+                                 'the method call as an argument, or when '
+                                 'instantiating the class')
+        if cache_path is None:
+            cache_path = self.cache_path
+        return output_path, cache_path
+
+    
+    def clear_cache(self):
+        path = os.path.join(self.cache_path, self.dataset_name)
+        warnings.warn(f'Deleting existing directory: {path}')
+        shutil.rmtree(path)
+        self.downloaded = False
+        self.extracted = False
+    
+    
+    def download_midi(self, output_path=None, cache_path=None):
+        """Downloads the MIDI data to output_path"""
+        output_path, cache_path = self.get_output_and_cache_path(
                 'midi',
-                output_loc,
-                cache_loc
+                output_path,
+                cache_path
             )
         raise NotImplementedError('In order to download MIDI, you must '
                                   'implement the download_midi method.')
         
     
-    def download_csv(self, output_loc=None, cache_loc=None):
-        """Downloads the csv data to output_loc"""
-        output_loc, cache_loc = self.get_output_and_cache_loc(
+    def download_csv(self, output_path=None, cache_path=None):
+        """Downloads the csv data to output_path"""
+        output_path, cache_path = self.get_output_and_cache_path(
                 'csv',
-                output_loc,
-                cache_loc
+                output_path,
+                cache_path
             )
         raise NotImplementedError('In order to download CSV, you must '
                                   'implement the download_midi method.')
+        
         
 
 # TODO: maybe make a base PPDD class and extend this for various options
@@ -88,49 +175,71 @@ class PPDDSept2018Monophonic(DataDownloader):
     ----------
     https://www.music-ir.org/mirex/wiki/2019:Patterns_for_Prediction
     """
-    def __init__(self, cache_loc=DEFAULT_CACHE_LOC, 
-                 midi_output_loc=None, csv_output_loc=None):
+    def __init__(self, cache_path=DEFAULT_CACHE_PATH, 
+                 midi_output_path=None, csv_output_path=None,
+                 sizes=['small', 'medium', 'large'],
+                 clean=False):
         self.dataset_name = self.__class__.__name__
         base_url = ('http://tomcollinsresearch.net/research/data/mirex/'
                          'ppdd/ppdd-sep2018')
-        sizes = ['small', 'medium', 'large']
         self.download_urls = [os.path.join(base_url, 
                                            f'PPDD-Sep2018_sym_mono_{size}.zip')
                               for size in sizes]
-        self.cache_loc = cache_loc
-        if not os.path.exists(cache_loc):
-            # TODO: handle overwriting?
-            os.mkdir(cache_loc)
-        self.output_loc = {'midi': midi_output_loc, 'csv': csv_output_loc}
-    
-    
-    def download_and_extract_zips(self, cache_loc=None, overwrite=None):
-        cache_loc = self.cache_loc if cache_loc is None else cache_loc
-        base_path = os.path.join(cache_loc, self.dataset_name)
-        try:
-            os.mkdir(base_path)
-        except FileExistsError as e:
-            if overwrite is True:
-                print(f'Deleting existing directory: {base_path}')
-                shutil.rmtree(base_path)
-                os.mkdir(base_path)
-            elif overwrite is None:
-                print(f'WARNING: {base_path} already exists, writing files '
-                       'within here only if they do not already exist.')
-            elif overwrite is False:
-                raise e
-            else:
-                raise ValueError('overwrite should be boolean or None, not '
-                                 f'"{overwrite}"')
-        for url in self.download_urls:
-            filename = os.path.basename(url)
-            self.download_file(url, os.path.join(base_path, filename),
-                               overwrite=overwrite)
+        self.cache_path = cache_path
+        make_directory(cache_path, overwrite=None)
+        # location of midi files relative to each unzipped directory
+#        self.midi_paths = ['prime_midi', 'cont_true_midi']
+        self.midi_paths = ['prime_midi']
+        self.output_path = {'midi': midi_output_path, 'csv': csv_output_path}
+        self.downloaded = False
+        self.extracted = False
+        self.clean = clean
         
     
-    
-    
-    
+    def download_midi(self, output_path=None, cache_path=None, overwrite=None):
+        # Cleaning paths, and setting up cache dir ============================
+        output_path, cache_path = self.get_output_and_cache_paths(
+                'midi',
+                output_path=output_path,
+                cache_path=cache_path
+            )
+        base_path = os.path.join(cache_path, self.dataset_name)
+        make_directory(base_path, overwrite)
+        make_directory(output_path, overwrite)
+        
+        # Downloading the data ================================================
+        if (not self.downloaded) or overwrite is not False:
+            zip_paths = []
+            for url in self.download_urls:
+                filename = os.path.basename(url)
+                zip_path = os.path.join(base_path, filename)
+                zip_paths += [zip_path]
+                download_file(url, zip_path, overwrite=overwrite)
+            self.downloaded = True
+        
+        # Extracting data from zip files ======================================
+        extracted_paths = []
+        if (not self.extracted) or overwrite is not False:
+            for zip_path in zip_paths:
+                path = extract_zip(zip_path, base_path, overwrite=overwrite)
+                extracted_paths += [path]
+            self.extracted = True
+        
+        # Copying midi files to output_path ===================================
+        for path in extracted_paths:
+            midi_paths = [glob.glob(os.path.join(path, mp, '*')) for mp
+                          in self.midi_paths]
+            midi_paths = [pp for sublist in midi_paths for pp in sublist]
+            for filepath in tqdm(midi_paths, 
+                                 desc=f"Copying midi from {path}: "):
+                shutil.copy(filepath, output_path)
+        
+        # Delete cache ========================================================
+        if self.clean:
+            self.clear_cache()
+        
+        
+        
 
 class PPDDSept2018Polyphonic(PPDDSept2018Monophonic):
     pass
