19b43602e43cefe6a031d113ebcf456f8d6da931
JamesOwers <james.f.owers+github@gmail.com>
2019-08-01 11:35:11 +0100

    Change function of get_degradations
    As it was coded, get_degradations didn't really have any utility
    over storing a dict (like mdtk.degrations.DEGRADATIONS now). I've
    changed the function to return the functions from a list of strings.

--- mdtk/degradations.py
+++ mdtk/degradations.py
@@ -4,37 +4,19 @@ import numpy as np
 from mdtk.data_structures import Composition
 
 
-def get_degradations():
-    """
-    Get a dict mapping the name of each degradation to its function pointer.
-    
-    Returns
-    -------
-    degradations : dict(string -> func)
-        A dict containing the name of each degradation mapped to a pointer to
-        the method which performs that degradation.
-    """
-    return {'pitch_shift': pitch_shift,
-            'time_shift': time_shift,
-            'onset_shift': onset_shift,
-            'offset_shift': offset_shift,
-            'remove_note': remove_note,
-            'add_note': add_note}
-
-
 
 def pitch_shift(excerpt, rand, params):
     """
     Shift the pitch of one note from the given excerpt.
-    
+
     Parameters
     ----------
     excerpt : Composition
         A Composition object of an excerpt from a piece of music.
-        
+
     rand : numpy.random
         A seeded numpy random object.
-        
+
     params : dict
         A dictionary containing parameters for the pitch shift. All used
         parameters keys will begin with 'pitch_shift_'. They include:
@@ -44,7 +26,7 @@ def pitch_shift(excerpt, rand, params):
             max_pitch : int
                 One greater than the maximum pitch to which a note may be
                 shifted. Defaults to 88.
-            
+
     Returns
     -------
     degraded : Composition
@@ -54,17 +36,17 @@ def pitch_shift(excerpt, rand, params):
                  else params['pitch_shift_min_pitch'])
     max_pitch = (88 if 'pitch_shift_max_pitch' not in params
                  else params['pitch_shift_max_pitch'])
-    
+
     degraded = excerpt.copy()
-    
+
     # Sample a random note
     note_index = rand.randint(0, degraded.note_df.shape[0])
-    
+
     # Shift its pitch (to something new)
     original_pitch = degraded.note_df.loc[note_index, 'pitch']
     while degraded.note_df.loc[note_index, 'pitch'] == original_pitch:
         degraded.note_df.loc[note_index, 'pitch'] = rand.randint(min_pitch, max_pitch)
-        
+
     return degraded
 
 
@@ -73,20 +55,20 @@ def time_shift(excerpt, rand, params):
     """
     Shift the onset and offset times of one note from the given excerpt,
     leaving its duration unchanged.
-    
+
     Parameters
     ----------
     excerpt : Composition
         A Composition object of an excerpt from a piece of music.
-        
+
     rand : numpy.random
         A seeded numpy random object.
-        
+
     params : dict
         A dictionary containing parameters for the time shift. All used
         parameters keys will begin with 'time_shift_'. They include:
             None
-            
+
     Returns
     -------
     degraded : Composition
@@ -98,20 +80,20 @@ def time_shift(excerpt, rand, params):
 def onset_shift(excerpt, rand, params):
     """
     Shift the onset time of one note from the given excerpt.
-    
+
     Parameters
     ----------
     excerpt : Composition
         A Composition object of an excerpt from a piece of music.
-        
+
     rand : numpy.random
         A seeded numpy random object.
-        
+
     params : dict
         A dictionary containing parameters for the onset shift. All used
         parameters keys will begin with 'onset_shift_'. They include:
             None
-            
+
     Returns
     -------
     degraded : Composition
@@ -123,20 +105,20 @@ def onset_shift(excerpt, rand, params):
 def offset_shift(excerpt, rand, params):
     """
     Shift the offset time of one note from the given excerpt.
-    
+
     Parameters
     ----------
     excerpt : Composition
         A Composition object of an excerpt from a piece of music.
-        
+
     rand : numpy.random
         A seeded numpy random object.
-        
+
     params : dict
         A dictionary containing parameters for the offset shift. All used
         parameters keys will begin with 'offset_shift_'. They include:
             None
-            
+
     Returns
     -------
     degraded : Composition
@@ -148,33 +130,33 @@ def offset_shift(excerpt, rand, params):
 def remove_note(excerpt, rand, params):
     """
     Remove one note from the given excerpt.
-    
+
     Parameters
     ----------
     excerpt : Composition
         A Composition object of an excerpt from a piece of music.
-        
+
     rand : numpy.random
         A seeded numpy random object.
-        
+
     params : dict
         A dictionary containing parameters for the note removal. All used
         parameters keys will begin with 'remove_note_'. They include:
             None
-            
+
     Returns
     -------
     degraded : Composition
         A copy of the given excerpt, with one note removed.
     """
     degraded = excerpt.copy()
-    
+
     # Sample a random note
     note_index = rand.randint(0, degraded.note_df.shape[0])
-    
+
     # Remove that note
     degraded.note_df.drop(0, inplace=True).reset_index(drop=True, inplace=True)
-        
+
     return degraded
 
 
@@ -182,15 +164,15 @@ def remove_note(excerpt, rand, params):
 def add_note(excerpt, rand, params):
     """
     Add one note to the given excerpt.
-    
+
     Parameters
     ----------
     excerpt : Composition
         A Composition object of an excerpt from a piece of music.
-        
+
     rand : numpy.random
         A seeded numpy random object.
-        
+
     params : dict
         A dictionary containing parameters for the note addition. All used
         parameters keys will begin with 'add_note_'. They include:
@@ -206,7 +188,7 @@ def add_note(excerpt, rand, params):
                 The maximum duration for the added note. Defaults to infinity.
                 (The offset time will never go beyond the current last offset
                 in the excerpt.)
-            
+
     Returns
     -------
     degraded : Composition
@@ -220,26 +202,53 @@ def add_note(excerpt, rand, params):
                     else params['add_note_min_duration'])
     max_duration = (np.inf if 'add_note_max_duration' not in params
                     else params['add_note_max_duration'])
-    
+
     degraded = excerpt.copy()
-    
+
     end_time = degraded.note_df[['onset', 'dur']].sum(axis=1).max()
-    
+
     pitch = rand.randint(min_pitch, max_pitch)
-    
+
     onset = rand.uniform(degraded.note_df['onset'].min(), end_time - min_duration)
     duration = rand.uniform(onset + min_duration, max(end_time - onset, max_duration))
-    
+
     # Track is random one of existing tracks
     track = rand.choice(degraded.note_df['track'].unique())
-    
+
     degraded.note_df = degraded.note_df.append({'pitch': pitch,
                                                 'onset': onset,
                                                 'dur': duration,
                                                 'track': track}, ignore_index=True)
-    
+
     return degraded
-    
-    
 
 
+
+DEGRADATIONS = {
+    'pitch_shift': pitch_shift,
+    'time_shift': time_shift,
+    'onset_shift': onset_shift,
+    'offset_shift': offset_shift,
+    'remove_note': remove_note,
+    'add_note': add_note
+}
+
+
+
+def get_degradations(degradation_list):
+    """
+    A convenience function to get degradation functions from strings.
+
+    Parameters
+    ----------
+    degradation_list : list of strings
+        list containing the names of the degradation functions to get
+
+    Returns
+    -------
+    degradations : dict(string -> func)
+        A dict containing the name of each degradation mapped to a pointer to
+        the method which performs that degradation.
+    """
+    deg_funcs = [DEGRADATIONS[deg_str] for deg_str in degradation_list]
+    return deg_funcs
