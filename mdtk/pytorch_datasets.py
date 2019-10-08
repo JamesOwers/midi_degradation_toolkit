638ac961983efcdacdf9c6a14ed229dd24aa9c0b
JamesOwers <james.f.owers+github@gmail.com>
2019-10-08 23:33:47 +0100

    First draft of dataset, and corpus creation functions
    Dataset is made to read from a text corpus in a similar way to
    https://github.com/codertimo/BERT-pytorch/blob/master/bert_pytorch/dataset/dataset.py

--- mdtk/pytorch_datasets.py
+++ mdtk/pytorch_datasets.py
@@ -1,11 +1,38 @@
 """classes to use in conjunction with pytorch dataloaders"""
+from torch.utils.data import Dataset
+import tqdm
+import torch
+import random
 import pandas as pd
 import numpy as np
 
 #TODO: probably want to move everything but Pytorch dataset objects out of here
 
+# Convenience function...
+def diff_pd(df1, df2):
+    """Identify differences between two pandas DataFrames"""
+    assert (df1.columns == df2.columns).all(), \
+        "DataFrame column names are different"
+    if any(df1.dtypes != df2.dtypes):
+        "Data Types are different, trying to convert"
+        df2 = df2.astype(df1.dtypes)
+    if df1.equals(df2):
+        return None
+    else:
+        # need to account for np.nan != np.nan returning True
+        diff_mask = (df1 != df2) & ~(df1.isnull() & df2.isnull())
+        ne_stacked = diff_mask.stack()
+        changed = ne_stacked[ne_stacked]
+        changed.index.names = ['id', 'col']
+        difference_locations = np.where(diff_mask)
+        changed_from = df1.values[difference_locations]
+        changed_to = df2.values[difference_locations]
+        return pd.DataFrame({'from': changed_from, 'to': changed_to},
+                            index=changed.index)
 
-# TODO: later can auto detect vocab from corpus
+# TODO: later can auto detect vocab from corpus if necessary
+#       I'm doing things this way just for ability to change things
+#       later with ease
 class CommandVocab(object):
     def __init__(self, min_pitch=0,
                  max_pitch=127,
@@ -53,8 +80,8 @@ def df_to_command_str(df, min_pitch=0, max_pitch=127, time_increment=40,
 
     Returns
     -------
-    one_hot : np.ndarray
-        An array containing the one-hot vectors describing the given DataFrame.
+    command_string : str
+        The string containing a space separated list of commands.
     """
     # Input validation
     assert max_time_shift % time_increment == 0, ("max_time_shift must be "
@@ -76,7 +103,7 @@ def df_to_command_str(df, min_pitch=0, max_pitch=127, time_increment=40,
     
     command_list = []
     current_onset = commands.onset.iloc[0]
-    for idx, row in commands.iteritems():
+    for idx, row in commands.iterrows():
         time_shift = row.onset - current_onset
         if time_shift != 0:
             command_list += [f't{time_shift}']
@@ -84,3 +111,173 @@ def df_to_command_str(df, min_pitch=0, max_pitch=127, time_increment=40,
         current_onset = row.onset
 
     return ' '.join(command_list)
+
+
+def command_str_to_df(cmd_str):
+    """
+    Convert a given string of commands back to a pandas DataFrame.
+
+    Parameters
+    ----------
+    cmd_str : str
+        The string containing a space separated list of commands.
+
+    Returns
+    -------
+    df : pd.DataFrame
+        The pandas DataFrame representing the note data
+    """    
+    commands = cmd_str.split()
+    note_on_pitch = []
+    note_on_time = []
+    note_off_pitch = []
+    note_off_time = []
+    curr_time = 0
+    for cmd_str in commands:
+        cmd = cmd_str[0]
+        value = int(cmd_str[1:])
+        if cmd == 'o':
+            note_on_pitch += [value]
+            note_on_time += [curr_time]
+        elif cmd == 'f':
+            note_off_pitch += [value]
+            note_off_time += [curr_time]
+        elif cmd == 't':
+            curr_time += value
+        else:
+            raise ValueError(f'Invalid command {cmd}')
+    df = pd.DataFrame(columns=['onset', 'track', 'pitch', 'dur'], dtype=int)
+    for ii, (pitch, onset) in enumerate(zip(note_on_pitch, note_on_time)):
+        note_off_idx = note_off_pitch.index(pitch)  # gets first instance
+        note_off_pitch.pop(note_off_idx)
+        off = note_off_time.pop(note_off_idx)
+        dur = off - onset
+        track = 0
+        df.loc[ii] = [onset, track, pitch, dur]
+    
+    return df
+    
+
+
+def transform_to_torchtensor(output):
+    return {key: torch.tensor(value) for key, value in output.items()}
+
+
+
+# This is adapted from https://github.com/codertimo/BERT-pytorch/blob/master/bert_pytorch/dataset/dataset.py
+class CommandDataset(Dataset):
+    def __init__(self, corpus_path, vocab, seq_len, encoding="utf-8",
+                 corpus_lines=None, in_memory=True, transform=None):
+        """
+        Returns data required for ACME tasks. The returned item
+    
+        Parameters
+        ----------
+        corpus_path : str
+            Path to document containing the corpus of data. Each line is comma
+            separated and contains the degraded command string, clean command
+            string, then the degadation id label (0 is no degradation).
+            
+        vocab : Vocab class
+            A Vocab class object (see CommandVocab above). This is used to
+            convert the string commands to integers and serves as an easy
+            way of getting them back again.
+        
+        seq_len : int
+            The maximum length for a sequence (all sequences will be padded
+            to this length)
+        
+        encoding : str
+            Encoding to use when opening the corpus file.
+        
+        corpus_lines : int
+            Optional, if you know the number of lines in the corpus, supplying
+            the number here saves counting the lines in the file.
+        
+        in_memory : bool
+            Whether to store data in memory, or read from disk. N.B. If reading
+            from disk, the __get_item__ method ignores the item index and just
+            reads the next line of the file. This means batching will not be
+            random when used with a dataloader.
+        
+        transform: func
+            The output from __get_item__ is a dictionary of numpy arrays.
+            The function transform is applied to the dictionary before it is
+            returned so, for example, it can be used to convert all data to
+            torch tensors.
+    
+        Returns
+        -------
+        df : pd.DataFrame
+            The pandas DataFrame representing the note data
+        """    
+        self.vocab = vocab
+        self.seq_len = seq_len
+
+        self.in_memory = in_memory
+        self.corpus_lines = corpus_lines
+        self.corpus_path = corpus_path
+        self.encoding = encoding
+        
+        self.transform = transform
+        
+        with open(corpus_path, "r", encoding=encoding) as f:
+            if self.corpus_lines is None and not in_memory:
+                for _ in tqdm.tqdm(f, desc="Counting nr corpus lines"):
+                    self.corpus_lines += 1
+
+            if in_memory:
+                self.lines = [line[:-1].split(",")
+                              for line in tqdm.tqdm(f, desc="Loading Dataset",
+                                                    total=corpus_lines)]
+                self.corpus_lines = len(self.lines)
+
+        if not in_memory:
+            self.file = open(corpus_path, "r", encoding=encoding)
+
+    def __len__(self):
+        return self.corpus_lines
+
+    def __getitem__(self, item):
+        deg_cmd, clean_cmd, deg_num = self.get_corpus_line(item)
+        deg_cmd = self.tokenize_sentence(deg_cmd)
+        deg_cmd = [self.vocab.sos_index] + deg_cmd + [self.vocab.eos_index]
+        clean_cmd = self.tokenize_sentence(clean_cmd)
+        clean_cmd = [self.vocab.sos_index] + clean_cmd + [self.vocab.eos_index]
+        deg_num = int(deg_num)
+        
+        deg_cmd = deg_cmd[:self.seq_len]
+        deg_cmd += [self.vocab.pad_index for _ in 
+                    range(self.seq_len - len(deg_cmd))]
+        clean_cmd = clean_cmd[:self.seq_len]
+        clean_cmd += [self.vocab.pad_index for _ in 
+                      range(self.seq_len - len(clean_cmd))]
+        
+        output = {"deg_commands": deg_cmd,
+                  "clean_commands": clean_cmd,
+                  "deg_label": deg_num}
+        
+        # TODO: implement transform as in https://pytorch.org/tutorials/beginner/data_loading_tutorial.html
+        if self.transform is not None:
+            output = self.transform(output)
+        return output 
+
+    def tokenize_sentence(self, sentence):
+        tokens = sentence.split()
+        for ii, token in enumerate(tokens):
+            tokens[ii] = self.vocab.stoi.get(token, self.vocab.unk_index)
+        return tokens
+
+    def get_corpus_line(self, item):
+        if self.in_memory:
+            deg_cmd, clean_cmd, deg_num = self.lines[item]
+            return deg_cmd, clean_cmd, deg_num
+        else:
+            line = self.file.__next__()
+            if line is None:
+                self.file.close()
+                self.file = open(self.corpus_path, "r", encoding=self.encoding)
+                line = self.file.__next__()
+
+            deg_cmd, clean_cmd, deg_num = line[:-1].split(",")
+            return deg_cmd, clean_cmd, deg_num
