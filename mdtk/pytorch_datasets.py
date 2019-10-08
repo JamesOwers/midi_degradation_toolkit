94b918d93806b42d23788697cdfe578961d6cd8e
Andrew McLeod <apmcleod@gmail.com>
2019-10-08 16:08:29 +0900

    df_to_one_hots seems to work

--- mdtk/pytorch_datasets.py
+++ mdtk/pytorch_datasets.py
@@ -19,7 +19,7 @@ def df_to_one_hots(df, min_pitch=0, max_pitch=127, frame_length=40,
     max_pitch : int
         The maximum pitch at which notes will occur.
 
-    frame_length : int
+    frame_length : float
         The length of a single frame, in milliseconds.
 
     max_shift : int
@@ -39,33 +39,39 @@ def df_to_one_hots(df, min_pitch=0, max_pitch=127, frame_length=40,
     assert max_shift > 0, "max_shift must be positive"
 
     num_pitches = max_pitch - min_pitch + 1
-    num_shifts = max_shift / frame_length
+    num_shifts = round(max_shift // frame_length)
     # note_on, note_off, shift, EOF
     vector_length = num_pitches * 2 + num_shifts + 1
 
-    ONSET = 0
-    OFFSET = 1
+    OFFSET = 0
+    ONSET = 1
 
     # Get all note on and offs
     note_actions = []
     offsets = df['onset'] + df['dur']
-    for note, offset in zip(df.iterrows(), offsets):
-        note_actions.append((convert_time(note['onset'], frame_length),
-                             ONSET, note['pitch']))
-        note_actions.append((convert_time(offset, frame_length),
-                             OFFSET, note['pitch']))
+    for (_, note), offset in zip(df.iterrows(), offsets):
+        onset_frame = round(note['onset'] / frame_length)
+        offset_frame = round(offset / frame_length)
+        if onset_frame == offset_frame:
+            warnings.warn(f"onset_frame ({onset_frame}) == offset_frame "
+                          f"({offset_frame}) for note {note}. Skipping note. "
+                          f"Try using a smaller frame_rate (currently "
+                          f"{frame_rate}).")
+        else:
+            note_actions.append((onset_frame, ONSET, note['pitch']))
+            note_actions.append((offset_frame, OFFSET, note['pitch']))
 
     # Sort the note actions by time, then on/off, then pitch
     note_actions = sorted(note_actions)
 
     # Create one-hot sequence
     one_hots = []
-    current_frame = note_actions[0][0]
+    current_frame = int(note_actions[0][0])
     for frame, action, pitch in note_actions:
         # Shift
         # TODO: Do we want to support iterative shifts like this or just error
         while frame != current_frame:
-            diff = min(num_shifts, frame - current_frame)
+            diff = int(min(num_shifts, frame - current_frame))
             shift_vector = np.zeros(vector_length)
             shift_vector[2 * num_pitches + diff] = 1
             one_hots.append(shift_vector)
@@ -76,8 +82,7 @@ def df_to_one_hots(df, min_pitch=0, max_pitch=127, frame_length=40,
             f"All pitches must be in range [{min_pitch}, {max_pitch}]."
         )
         action_vector = np.zeros(vector_length)
-        index = pitch if action == ONSET else pitch + num_pitches
-        action_vector[index] = 1
+        action_vector[action * num_pitches + pitch] = 1
         one_hots.append(action_vector)
 
     # Add EOF
