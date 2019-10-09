4c587652d3f2669f4fcf5e4174d2b99d351635e7
JamesOwers <james.f.owers+github@gmail.com>
2019-10-09 21:18:11 +0100

    Fix odd permutation of batch for LSTM

--- mdtk/pytorch_models.py
+++ mdtk/pytorch_models.py
@@ -36,17 +36,15 @@ class ErrorDetectionNet(nn.Module):
 
 
     def init_hidden(self, batch_size):
-        return (autograd.Variable(torch.randn(1, batch_size, self.hidden_dim)),
-                autograd.Variable(torch.randn(1, batch_size, self.hidden_dim)))
-
+        return (torch.randn(1, batch_size, self.hidden_dim),
+                torch.randn(1, batch_size, self.hidden_dim))
 
     def forward(self, batch):
-        
-        self.hidden = self.init_hidden(batch.size(-1))
-
-        embeds = self.embedding(batch)
+        batch_size = batch.shape[0]
+        self.hidden = self.init_hidden(batch_size)
+        # Weirdly have to permute batch dimension to second for LSTM...
+        embeds = self.embedding(batch).permute(1, 0, 2)
         outputs, (ht, ct) = self.lstm(embeds, self.hidden)
-
         # ht is the last hidden state of the sequences
         # ht = (1 x batch_size x hidden_dim)
         # ht[-1] = (batch_size x hidden_dim)
