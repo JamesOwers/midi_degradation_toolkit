6c5e0987f5c54d5ed2932e3b93ea2bea3204c40e
apmcleod <apmcleod@gmail.com>
2019-10-12 23:44:34 +0900

    Added framewise and notewise functions

--- mdtk/eval.py
+++ mdtk/eval.py
@@ -31,18 +31,93 @@ def helpfulness(corrected_df, degraded_df, clean_df, time_increment=40):
         -- Else, using 0.0 == 0.0, degraded == 0.5 and clean == 1.0 as anchor
            points, place corrected's score on that scale.
     """
-    # Use this for note-based F-measure
-    #precision_recall_f1_overlap(ref_intervals, ref_pitches, est_intervals, est_pitches, onset_tolerance=0.05, pitch_tolerance=50.0, offset_ratio=0.2, offset_min_tolerance=0.05, strict=False, beta=1.0)
-    corrected_fm = 0 # TODO: Get F-measure
-    # TODO: also get frame-based
+    corrected_fm = get_combined_fmeasure(corrected_df, clean_df,
+                                         time_increment=time_increment)
 
+    # Quick exit on border cases
     if corrected_fm == 0 or corrected_fm == 1 or degraded_df.equals(clean_df):
         return corrected_fm
 
-    degraded_fm = 0 # TODO: Calculate this
-    # TODO: Also get frame-based
+    degraded_fm = get_combined_fmeasure(degraded_df, clean_df,
+                                        time_increment=time_increment)
 
     if corrected_fm < degraded_fm:
         return 0.5 * corrected_fm / degraded_fm
     else:
         return 1 - 0.5 * (1 - corrected_fm) / (1 - degraded_fm)
+
+
+def get_combined_fmeasure(df, gt_df, time_increment=40):
+    """
+    Get the combined framewise and notewise F-measure of a dtaframe,
+    given a ground truth dataframe.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The dataframe whose F-measure to return.
+
+    gt_df : pd.DataFrame
+        The ground truth dataframe.
+
+    time_increment : int
+        The length of a frame to use when performing evaluations.
+
+    Returns
+    -------
+    f_measure : float
+        The combined f_measure of the given dataframe. The mean of its
+        framewise and notewise F-measures.
+     """
+    fw = get_framewise_f_measure(df, gt_df, time_increment=time_increment)
+    nw = get_notewise_f_measure(df, gt_df, time_increment=time_increment)
+    return (fw + nw) / 2
+
+
+def get_framewise_f_measure(df, gt_df, time_increment=40):
+    """
+    Get the framewise F-measure of a dtaframe, given a ground truth dataframe.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The dataframe whose framewise F-measure to return.
+
+    gt_df : pd.DataFrame
+        The ground truth dataframe.
+
+    time_increment : int
+        The length of a frame to use when performing evaluations.
+
+    Returns
+    -------
+    f_measure : float
+        The framewise f_measure of the given dataframe.
+    """
+    raise NotImplementedError()
+ 
+ 
+ def get_notewise_f_measure(df, gt_df, time_increment=40):
+    """
+    Get the notewise F-measure of a dtaframe, given a ground truth dataframe.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        The dataframe whose notewise F-measure to return.
+
+    gt_df : pd.DataFrame
+        The ground truth dataframe.
+
+    time_increment : int
+        The length of a frame to use for quantization when performing
+        evaluations.
+
+    Returns
+    -------
+    f_measure : float
+        The notewise f_measure of the given dataframe, using mir_eval.
+    """
+    raise NotImplementedError()
+    # Use this for note-based F-measure
+    #precision_recall_f1_overlap(ref_intervals, ref_pitches, est_intervals, est_pitches, onset_tolerance=0.05, pitch_tolerance=50.0, offset_ratio=0.2, offset_min_tolerance=0.05, strict=False, beta=1.0)
