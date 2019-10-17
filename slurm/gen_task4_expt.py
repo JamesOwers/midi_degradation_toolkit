cbb74f12074d1026cb95bc4be2954c286e292369
jamesowers <james.f.owers+github@gmail.com>
2019-10-17 15:47:58 +0100

    Added new options for big re-run

--- slurm/gen_task4_expt.py
+++ slurm/gen_task4_expt.py
@@ -12,8 +12,6 @@ in_dir = '/disk/scratch/s0816700/data/acme'
 
 epochs = 10000
 early_stopping = 50
-layers = [128, 64, 32]
-lay1, lay2, lay3 = layers
 base_call = (
     f"python {code_dir}/baselines/train_task.py "
     f"--task {task_nr} "
@@ -23,36 +21,37 @@ base_call = (
     f"--epochs {epochs} "
     f"--batch_log_freq None "
     f"--early_stopping {early_stopping} "
-    f"--layers {lay1} {lay2} {lay3}"
 )
 
 nr_repeats = 3
 learning_rates = [1e-5, 1e-4]
 weight_decays = [1e-2, 1e-3]
 hiddens = [100, 250]
-nr_expts = nr_repeats * len(learning_rates) * len(weight_decays) * len(hiddens)
+layers = [[128, 64, 32], [250, 250], [250]]
+nr_expts = nr_repeats * len(learning_rates) * len(weight_decays) * len(hiddens) * len(layers)
 
 nr_servers = 10
 avg_expt_time = 60  # mins
 print(f'Total experiments = {nr_expts}')
 print(f'Estimated time = {(nr_expts / nr_servers * avg_expt_time)/60} hrs')
 
-settings = [(lr, wd, hid, repeat) for lr in learning_rates 
+settings = [(lr, wd, hid, lay, repeat) for lr in learning_rates 
             for wd in weight_decays for hid in hiddens 
-            for repeat in range(nr_repeats)]
+            for lay in layers for repeat in range(nr_repeats)]
 
 output_file = open(f"{code_dir}/slurm/experiments/"
                    f"{high_level_expt_name}_experiments.txt", "w")
-for lr, wd, hid, repeat in settings:
-    expt_name = f"{high_level_expt_name}__{lr}_{wd}_{hid}_{repeat}"
+for lr, wd, hid, lay, repeat in settings:
+    lay_str = ' '.join([str(ll) for ll in lay])
+    expt_name = f"{high_level_expt_name}__{lr}_{wd}_{hid}_{lay_str.replace(' ', '-')}_{repeat}"
     log_file = f"{out_dir}/{expt_name}.log"
     model_outpath = f"{out_dir}/{expt_name}.checkpoint"
-    
     expt_call = (
         f"{base_call} "
         f"--lr {lr} "
         f"--weight_decay {wd} "
         f"--hidden {hid} "
+        f"--layers {lay_str} "
         f"--output {model_outpath} "
         f"--log_file {log_file}"
     )
